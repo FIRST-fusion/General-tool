a0cbc44d656c10a57f4cf7895f5c02ced662e2d0
sun51027 <linshih010@gmail.com>
2024-03-29 16:05:36 +0800

    update py multiprocessing interface

--- run_dcon_singlefile.sh
+++ run_dcon_singlefile.sh
@@ -1,13 +1,26 @@
+# This script is created by Lin Shih
+# Date : 2023 8 12
+# After running vmec, execute this script with dcon_{filename}.txt output from vmec
+#
+#       ./run_dcon_singlefile.sh dcon_{filename}.txt
+#
+# The script will examine the stability from n = 1 to n = 5 mode.
+# You can change the mode range in the forloop.
+# Note:
+#  Before you run this script:
+#       i. modify the eq_type from the default one to 'vmec' in "equil.in" inside the dcon_dir
+#       ii. modify dcon_dir to your own path of dcon directory
+#
+# Contact: Lin Shih (linshih010@gmail.com)
+
 #!/bin/bash
 
-# single file
 equil_file=${1}
 
-# Store the current directory
+# dcon path 
 current_dir=$(pwd)
 dcon_dir="/home/linshih/workspace/dcon_3.80/dcon_3.80/rundir/Linux"
 
-# Change directory to where "dcon" is located
 cp ${equil_file} ${dcon_dir}
 cd ${dcon_dir}
 echo "befor change path and file >>> "
@@ -19,7 +32,8 @@ sed -i "s|eq_filename=.*|eq_filename=\"${filename}\"|" ${dcon_dir}/equil.in
 echo "after change path and file >>> "
 cat ${dcon_dir}/equil.in | grep eq_filename
 
-for n in {1..1}
+echo "Examine mode n = 1 to 5"
+for n in {1..5}
 do
 	sed -i "s|nn=.*|nn=${n}|" ${dcon_dir}/dcon.in
 	./dcon 
@@ -30,6 +44,4 @@ done
 # Run "dcon" with the current file as an argument
 #./xdraw dcon
 
-# Change back to the original directory
-#cd "$current_dir"
 
