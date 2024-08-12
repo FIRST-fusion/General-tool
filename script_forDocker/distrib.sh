b2e15ae3571cd490905aefa410bc6276285b4712
sun51027 <linshih010@gmail.com>
2024-08-12 08:47:56 +0800

    add executable dir

--- script_forDocker/distrib.sh
+++ script_forDocker/distrib.sh
@@ -1,25 +1,39 @@
-#
+#!/bin/bash
+
 # The script is to streamline the workflow between VMEC, TERPSICHORE, and DCON.
 # The paths below are only for docker.
 # If not using docker, you have to change all path in every scrips to your own usage.
-# 
-# All scripts here arec reated by Lin Shih 
+#
+# All scripts here arec reated by Lin Shih
 # email: linshih010@gmail.com
-# 
+#
 # 2024/08/09
 
 current_dir=$(pwd)
+exe_dir="/workspace/General-tool/script_forDocker"
 vmec_path="/workspace/Stellarator-Tools/build/_deps/parvmec-build"
 terp_path="/workspace/TERPSICHORE/TERPSICHORE_main"
 bsj_path="/workspace/TERPSICHORE/terp_bootsj"
 
-echo "cp bootsj_quick.sh run_dcon_singlefile.sh terps_quick.sh descur2vmec.py pcurr_generator.py vm3_quick.sh to VMEC run directory"
-cp ${current_dir}/bootsj_quick.sh \
-   ${current_dir}/run_dcon_singlefile.sh \
-   ${current_dir}/terps_quick.sh \
-   ${current_dir}/descur2vmec.py \
-   ${current_dir}/pcurr_generator.py \
-   ${current_dir}/vm3_quick.sh  \
-   ${vmec_path}
-echo "cp fort43_generator.sh to terp_bsj run directory"
-cp ${current_dir}/fort43_generator.sh ${bsj_path}
+if [ "$current_dir" == "$exe_dir" ]; then
+
+        echo "cp bootsj_quick.sh \
+              run_dcon_singlefile.sh \
+              terps_quick.sh \
+              descur2vmec.py \
+              pcurr_generator.py \
+              vm3_quick.sh to VMEC run directory"
+        cp ${current_dir}/bootsj_quick.sh \
+           ${current_dir}/run_dcon_singlefile.sh \
+           ${current_dir}/terps_quick.sh \
+           ${current_dir}/descur2vmec.py \
+           ${current_dir}/pcurr_generator.py \
+           ${current_dir}/vm3_quick.sh  \
+           ${vmec_path}
+        echo "cp fort43_generator.sh to terp_bsj run directory"
+        cp ${current_dir}/fort43_generator.sh ${bsj_path}
+else
+        echo "You are not executable directory"
+        echo "Gone to execute dir automatically!"
+        cd $exe_dir
+fi
